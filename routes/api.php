696db94d9906a65b1fedd20dc4026c2a0be846c5
Jahaslito <53540587+Jahaslito@users.noreply.github.com>
2021-11-30 23:48:05 +0300

    Update api.php
    Fixed merge changes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StaffRegistration;
+use App\Http\Controllers\MainController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
